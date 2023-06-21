b0f5959a67795132ee2a69ec2989daf775464b01
agcampos01 <agcampos@frba.utn.edu.ar>
2023-06-21 18:43:32 -0300

    Update main.cpp

--- 01-CalcuarPi/main.cpp
+++ 01-CalcuarPi/main.cpp
@@ -6,21 +6,24 @@ using namespace std;
 static double PI = 3.141592;
 
 double CalculoPi(){
+    int i=1;
     int n=0;
     int x=0;
     double dato=0;
-    for(int i=1; i<1000000 ;i++){
+    while(1){
         x = n % 2 ? -1 : 1;                  //si el exponente es par x = 1, si es impar x = -1
         dato += x / (double)(2*n+1);
         n++;
+        i++;
+        if(abs(dato*4-PI)<0.0000001)
+            break;
     }
-    //cout<<"Iteraciones: "<< n+1 <<"\tDato: "<< dato << endl;
+    //cout<<"Iteraciones: "<< i <<"\tDato: "<< dato << endl;
     return dato*4;
 }
 
 int main(){
     cout<<setprecision(6)<<fixed;
-    //cout<<"PI= "<< PI <<"\t PI/4= "<<  (PI/4) << endl;
     cout<<"Pi=\t"<< CalculoPi() << endl;
     return 0;
 }
