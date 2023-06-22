9c74b10cfe956de8e6e9034fb3be2f0a3ee3e5b1
AgCampos <agcampos@frba.utn.edu.ar>
2023-06-22 00:52:00 -0300

    Update main.cpp

--- 01-CalcuarPi/main.cpp
+++ 01-CalcuarPi/main.cpp
@@ -3,27 +3,28 @@
 
 using namespace std;
 
-static double PI = 3.141592;
-
 double CalculoPi(){
-    int i=1;
+    int cont=1;                              //Contador de iteraciones
     int n=0;
     int x=0;
     double dato=0;
+    double aux=0;
     while(1){
         x = n % 2 ? -1 : 1;                  //si el exponente es par x = 1, si es impar x = -1
         dato += x / (double)(2*n+1);
         n++;
-        i++;
-        if(abs(dato*4-PI)<0.0000001)
+        cont++;
+        if((abs(dato-aux))*4<0.000001)
             break;
+        else
+            aux=dato;
     }
-    //cout<<"Iteraciones: "<< i <<"\tDato: "<< dato << endl;
+    //cout<<"Iteraciones: "<< cont <<"\tDato: "<< dato << endl;
     return dato*4;
 }
 
 int main(){
     cout<<setprecision(6)<<fixed;
-    cout<<"Pi=\t"<< CalculoPi() << endl;
+    cout<<"Pi=\t"<< CalculoPi() << endl;        //El resultado se redondea y no se cómo cancelarlo :(
     return 0;
 }
